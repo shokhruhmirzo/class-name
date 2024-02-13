442d2e6067d3b8e7a3037826f8dc66ad6c26732f
shokhruhmirzo <shaalimov923@gmail.com>
2024-02-13 14:04:27 +0500

    add some things

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// var elArr = [
+//     {
+//         title : 'shoxa',
+//         text : 'lorem inpun sal lore',
+//         img : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D8k%2Bwallpaper&psig=AOvVaw0YWwNqco2EPbSvSi-PZQtp&ust=1707876444123000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKCmztedp4QDFQAAAAAdAAAAABAE'
+//     },
+//     {
+//         title : 'abu',
+//         text : 'pubg oi saoida ',
+//         img : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wallpaperflare.com%2Fsearch%3Fwallpaper%3D8k&psig=AOvVaw0YWwNqco2EPbSvSi-PZQtp&ust=1707876444123000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKCmztedp4QDFQAAAAAdAAAAABAR'
+//     },
+//     {
+//         title : 'shoxa',
+//         text : 'pubg oi saoida ',
+//         img : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wallpaperflare.com%2Fsearch%3Fwallpaper%3D8k&psig=AOvVaw0YWwNqco2EPbSvSi-PZQtp&ust=1707876444123000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKCmztedp4QDFQAAAAAdAAAAABAR'
+//     }
+// ]
+
 var arr = ['shoxrux', 'nasibullo', 'abdulboriy','abdullox', 'ahror', 'muxammadali','azim', 'ilmidin', 'ali','suxrob','sirojiddin','aziz']
 var elWrapper = document.querySelector('.wrapper')
 for (var i = 0; i < arr.length; i++) {
@@ -13,7 +31,7 @@ for (var i = 0; i < arr.length; i++) {
     elWrapper.appendChild(newBox)
 }
 
-document.addEventListener('contextmenu', function (p) {
-    p.preventDefault();
-    alert("Kod ni ko'chirish mumkinmas !!!");
-});
+// document.addEventListener('contextmenu', function (p) {
+//     p.preventDefault();
+//     alert("Kod ni ko'chirish mumkinmas !!!");
+// });
